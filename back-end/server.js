e59b43d82b3c983bb55996903ab63ed261c12b6c
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-08-27 13:30:39 -0400

    still getting started

---
+++ back-end/server.js
@@ -0,0 +1,14 @@
+// DEPENDENCIES
+const app = require("./app.js");
+
+// CONFIGURATIION
+require("dotenv").config();
+
+// PORT 
+const PORT = process.env.PORT;
+
+// LISTEN 
+app.listen(PORT, () => {
+    console.log(`⛹🏿‍♀️ ⚽️  Balling on port ${PORT} 🎾 🏀`);
+})
+
