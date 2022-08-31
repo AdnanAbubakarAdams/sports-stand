83b0f44f08137fb7a178f538d2e7d3f41d1e55fd
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-08-31 13:45:00 -0400

    adding components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+// COMPONENTS
 import Navbar from './components/NavBar/Navbar';
+import BasketBall from './components/BasketBall/BasketBall';
 
 // PAGES 
 import Edit from './pages/Edit';
@@ -18,6 +20,7 @@ function App() {
         <Route path="/players/new" element={<New/>} />
         <Route path="/players/:id" element={<Show/>} />
         <Route path="/players/:id/edit" element={<Edit/>}/>
+        <Route path="/basketball" element={<BasketBall/>} />
       </Routes>
     </Router>
   );
