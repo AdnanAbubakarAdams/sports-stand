7f36b679043b43a454ba9029bfa445b732daba31
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-09-03 00:13:28 -0400

    debugging comments frontend

--- front-end/src/components/Comments/Comment.js
+++ front-end/src/components/Comments/Comment.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CommentForm from './CommentForm';
 
-const Comment = () => {
+const Comment = ({ comments, handleDelete, handleSubmit }) => {
+  const [viewEditForm, toggleEditForm] = useState(false);
+
+  const toggleView = () => {
+    toggleEditForm(!viewEditForm)
+  }
   return (
-    <div>Comment</div>
+    <div className='comments'>
+      <button onClick={toggleView}>Edit</button>
+      { viewEditForm ? (
+        <CommentForm
+        handleSubmit={handleSubmit}
+        commentsDetails={comments}
+        toggleView={toggleView}>
+
+        </CommentForm>
+      ) : (
+        <div>
+          <h4>
+            {comments.rating}
+          </h4>
+          <h5>{comments.commenter}</h5>
+          <p>{comments.comment}</p>
+          <button onClick={() => handleDelete(comments.id)}>Delete</button>
+        </div>
+      )}
+
+    </div>
   )
 }
 
--- front-end/src/components/Comments/Comments.js
+++ front-end/src/components/Comments/Comments.js
@@ -1,11 +1,81 @@
 import React,{ useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
+import axios from "axios";
+import CommentForm from './CommentForm';
 
 
 
-const Comments = () => {
+const Comments = (props) => {
+  const API = process.env.REACT_APP_API_URL;
+
+  const [commentss, setCommentss] = useState([]);
+
+  let { id } = useParams();
+  
+  // ADD COMMENTS 
+  const handleAdd = (newComment) => {
+    axios.post(`${API}/players/${id}/comments`, newComment)
+    .then((response) => {
+      setCommentss([response.data, ...commentss])
+    },
+    (error) => console.error(error)
+    )
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+
+  // DELETE COMMENT
+  const handleDelete = (commentId) => {
+    axios.delete(`${API}/players/${id}/comments/${commentId}`)
+    .then((response) => {
+      const copyCommentArr = [...commentss];
+      const indexDeleteComment = copyCommentArr.findIndex((comment) => {
+        return comment.id === id;
+      });
+      copyCommentArr.splice(indexDeleteComment, 1);
+      setCommentss(copyCommentArr);
+    },
+    (error) => console.log(error)
+    )
+    .catch((error) => console.log(error))
+  };
+
+  // EDIT COMMENT
+  const handleEdit = (updateComment) => {
+    axios.put(`${API}/players/${id}/comments/${updateComment.id}`, updateComment)
+    .then((response) => {
+      const copyCommentArr = [...commentss];
+      const indexUpdateComment = copyCommentArr.findIndex((comment) => {
+        return comment.id === updateComment.id;
+      });
+      copyCommentArr[indexUpdateComment] = response.data;
+      setCommentss(copyCommentArr)
+    })
+    .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    axios.get(`${API}/players/${id}/comments`)
+    .then((response) => {
+      console.log(response.data);
+      setCommentss(response.data);
+    });
+  }, [id, API]);
   return (
-    <div>Comments</div>
+    <section className='comments'>
+      <CommentForm handleSubmit={handleAdd}>
+        <h4>Drop A comment</h4>
+      </CommentForm>
+      {commentss.map((comments) => {
+        <Comment key={comments.id}
+        comments={comments}
+        handleDelete={handleDelete}
+        handleSubmit={handleEdit}
+        />
+      })}
+
+    </section>
   )
 }
 
