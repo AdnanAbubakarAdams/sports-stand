393e6d12100154a6bc2344eb88fef39ac5ec78cf
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-09-11 14:35:33 -0400

    pushing through

--- front-end/src/components/SportsNews/NewsData.js
+++ front-end/src/components/SportsNews/NewsData.js
@@ -7,7 +7,7 @@ const NewsData = (props) => {
     <li className='carditem'>
         <Link className='cardlink' to={props.path}>
             <figure className='cardpic' data-category={props.label}>
-                <img className='cardImg' alt='sports Image' src={props.src} />
+                <img className='cardImg' alt='sports' src={props.src} />
             </figure>
             <div className='cardinfo'>
                 <h5 className='cardtext'>{props.text}</h5>
